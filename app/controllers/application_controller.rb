3efa0d6d430d70999fa47f462c07e4054d2bbfff
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-20 00:51:56 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -36,10 +36,10 @@ class ApplicationController < Sinatra::Base
   end
 
   post "/login" do
-    user = User.find_by(username: params[:username])
+    @user = User.find_by(username: params[:username])
     if params[:username] == "" || params[:password] == ""
       redirect '/failure'
-    elsif user & user.authenticate(params[:password])
+    elsif @user & @user.authenticate(params[:password])
       redirect '/account'
     else
       redirect '/failure'
