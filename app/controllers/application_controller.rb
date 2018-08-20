c6190e221ea0451b5cb8f650f3a6519d341d298c
Alejandro Sabogal <asabogal.git@gmail.com>
2018-08-20 00:32:22 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -21,6 +21,7 @@ class ApplicationController < Sinatra::Base
       redirect '/failure'
     else
       User.create(username: params[:username], password: params[:password])
+      redirect '/login'
   end
 
   get '/account' do
